37657ac664878cb5270b4d8dd442257e1c812909
muhammadatiqueqayum <atiqueqayum@gmail.com>
2024-02-24 00:45:44 +0500

    add initial file

---
+++ python_files/03_operators_more.py
@@ -0,0 +1,32 @@
+x = 15
+y = 4
+z = 5.5
+
+# addition
+print(x + y)
+
+a  = "Atique Qayum"
+b = "Pakistan"
+print(a)
+print(a + b)    # addition
+print(x-y)      # subtraction
+print(x-z)      # ans will be float
+print(x*y)      # multiplication
+print(x/y)      # division
+
+
+print(x-z*4/y+3)   # DMAS # PEMDAS # BODMAS
+
+# update the variable
+x = 11
+y = 3
+z = 5.2
+# comparison opetators
+print(x > y)
+print(x < y)
+print(x == y)   # == is equal to 
+print(x != y)   # != is not equal to
+print(x >= y)   # >= is greater than or equal to
+print(x <= y)   # <= is less than or equal to
+print(x > y and x > z )
+print(x > y or x > z )
