f9c47253328b6f356511da3fe761302949f10ce4
muhammadatiqueqayum <atiqueqayum@gmail.com>
2024-02-23 23:30:34 +0500

    initial files

---
+++ python_files/01_operators.py
@@ -0,0 +1,20 @@
+print("we are learning python")
+print("hello world")   ## string
+print(3+4)             ## integer
+print(3.5 + 4.5)       ## float
+
+
+# operators
+
+a = [10, 11,12]
+b = [3, 5, 7]
+c = [10, 11, 12]
+print(a)
+print(b)
+
+print(a+b)   # concatinate
+print(a==b)  # compare
+print(a==c)  # compare boolean operator
+
+
+
---
+++ python_files/02_variable.py
@@ -0,0 +1,27 @@
+# variable 
+
+a = 5
+b = "Black Color"
+c = 5.5
+
+
+print(a)
+print(b)
+print(c)
+
+
+# use of variables and tips to use them
+
+# to perfoem calculation
+# to import the data and name that data
+
+
+# rules
+# always use small letters
+# do not use special characters
+# do not use spaces insted  use _
+# do not use captial letters inside the name Data
+# do not use functions which are builtin in python
+# do not use number at the begining or in middle, df_2 etc.
+# short names
+# descriptive 
