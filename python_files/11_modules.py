688db61e6e43adc47fe8e3a0544f637983ebb3f7
muhammadatiqueqayum <atiqueqayum@gmail.com>
2024-02-25 20:09:45 +0500

    add initial file

---
+++ python_files/11_modules.py
@@ -0,0 +1,9 @@
+# pip install pandas numpy matplotlib seaborn scipy stasmodels
+#import pandas as pd       
+
+
+
+#import math                # download complete library
+from math import sqrt       # only use single functions 
+
+print(sqrt(9))
---
+++ python_files/12_types_error.py
@@ -0,0 +1,33 @@
+
+#indentationError or syntex error
+# x = 5
+
+# if X > 3:
+#     print("X is positive value")
+
+
+# logical or human error
+
+# x = 5
+
+# if X > 3:
+#     print("y is positive value")
+
+
+# runtime or moduls not found error
+# # import numpy as np
+# print(np.sqrt(9))
+
+
+# syntac error
+# name error
+# type error
+# value error
+# index error
+# key error
+# attribute error
+# zero division error (1/0)
+# File not found error
+# import error
+# memory error
+# timeout error
